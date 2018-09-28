c3a268b08b447db52a3d596f00809820cd2ee9d4
Julian Forero <julian.forero1990@gmail.com>
2018-09-28 13:18:07 -0500

    Push

---
+++ backendninja2/src/main/java/com/udemy/backendninja/controller/LoginController.java
@@ -0,0 +1,36 @@
+package com.udemy.backendninja.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.udemy.backendninja.model.UserCredential;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/")
+	public String redirectToLogin() {
+		return "redirect:/login";
+	}
+
+	@GetMapping("/login")
+	public String showLoginForm(Model model,
+			@RequestParam(name = "error", defaultValue = "", required = false) String error,
+			@RequestParam(name = "logout", required = false) String logout) {
+		model.addAttribute("userCredentials", new UserCredential());
+		model.addAttribute("error", error);
+		model.addAttribute("logout", logout);
+		return "login";
+	}
+
+	@PostMapping("/logincheck")
+	public String loginCheck(@ModelAttribute("userCredentials") UserCredential userCredential) {
+		if (userCredential.getUsername().equals("user") && userCredential.getPassword().equals("user")) {
+			return "contacts";
+		}
+		return "redirect:/login?error=error";
+	}
+}
